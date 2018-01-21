d62d7075d0d298ba58b729ef987edbeba229f4bd
Sphrases <deraikoben@gmail.com>
2018-01-21 13:40:16 +0100

    scaling

--- HCI Web/index/JS/index.js
+++ HCI Web/index/JS/index.js
@@ -81,7 +81,33 @@ function hideEmojiSelector() {
 
 function startTimer() {
 
+}
+
+function scaleVideo() {
+    var viewportWidth = $(window).width();
+
+
+    var videoWidth = $("#videoElement").width();
+    var moveLeft = pos_to_neg(videoWidth/2) + viewportWidth/2;
+
+    console.log("video Breite " + videoWidth);
+    console.log("video Hälfte " + videoWidth/2);
+    console.log("VP Breite " + viewportWidth);
+    console.log("VP hälfte " + viewportWidth/2);
+    console.log(" ");
+    console.log("nach rechts um " + viewportWidth/2);
+    console.log("nach links um " + moveLeft);
 
 
 
+
+
+    $("#videoElement").css("left", viewportWidth/2)
+        .css("margin-left", moveLeft);
+}
+
+
+function pos_to_neg(num)
+{
+    return -Math.abs(num);
 }
