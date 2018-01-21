8a9ed40e728c92cba1a68ea071385d326aded93f
Sphrases <deraikoben@gmail.com>
2018-01-21 13:49:30 +0100

    scaling v2

--- HCI Web/index/JS/index.js
+++ HCI Web/index/JS/index.js
@@ -85,10 +85,9 @@ function startTimer() {
 
 function scaleVideo() {
     var viewportWidth = $(window).width();
-
-
     var videoWidth = $("#videoElement").width();
-    var moveLeft = pos_to_neg(videoWidth/2) + viewportWidth/2;
+
+    var moveLeft = pos_to_neg(videoWidth/2)+viewportWidth/2;
 
     console.log("video Breite " + videoWidth);
     console.log("video Hälfte " + videoWidth/2);
